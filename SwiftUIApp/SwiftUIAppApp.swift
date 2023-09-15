7df087194419bb39b0c481f269c983ec996bd94f
Rahul Serodia <O28802@bankfab.com>
2023-09-15 17:10:19 +0530

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Rahul Serodia on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
